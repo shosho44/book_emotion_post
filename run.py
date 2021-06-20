556476e6054ba4c60e35e10df0aadc263e1c1528
ssshotaro44 <shotaro2878@gmail.com>
2021-06-20 17:41:14 +0900

    投稿した内容をデータベースに登録して表示できるようにした

--- run.py
+++ run.py
@@ -1,11 +1,31 @@
 import flask
-from flask import Flask, Response, abort, render_template, url_for, flash, redirect
+from flask import Flask, Response, abort, render_template, url_for, flash, redirect, request
+from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, login_user, logout_user, login_required, UserMixin
 from collections import defaultdict
 from flask_login import login_user
 import flask_login
 
 app = flask.Flask(__name__)
+
+# データベースの設定
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.post_article'  # sqliteを使っている
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+
+class PostArticle(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.String(128), nullable=False)
+    post_content = db.Column(db.String(128), nullable=False)
+
+
+@app.route('/', methods=['GET', 'POST'])  # index関数を実行している
+def start_exe():
+    return render_template('index.html')
+
+"""
 app.secret_key = 'secret key'
 
 login_manager = flask_login.LoginManager()
@@ -65,7 +85,25 @@ def request_loader(request):
     user = User()
     user.user_id = user_id
     return user
+"""
+
+@app.route('/index')
+def index():
+    some_data = PostArticle.query.all()
+    return render_template('index.html', some_data=some_data)
+
+
+# 投稿した時の処理
+@app.route('/post_article', methods=['POST'])
+def post_article():
+    post_content = request.form['post-article']
+    tmp_user_id = '1'
+    some_data = PostArticle(user_id=tmp_user_id, post_content=post_content)
+    db.session.add(some_data)
+    db.session.commit()
+    return redirect(url_for('index'))
 
 
 if __name__ == '__main__':
+    db.create_all()
     app.run(host="0.0.0.0", port=8080, debug=True)
