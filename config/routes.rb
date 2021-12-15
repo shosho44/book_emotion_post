62007e3987708478a65c3d990c59dfcd042a29a2
ssshotaro44 <shotaro2878@gmail.com>
2021-12-15 19:14:19 +0900

    signupのviewを作成した

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :users do
+  resources :users, path_names: { new: 'signup' } do
     resource :bookmarks, { only: %i[show] } # TODO: bookmarks用のコントローラ作成
   end
 end
