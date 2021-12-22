f0da44c2e7e4d336cd5e63cc1d6d3c3a343ef86c
ssshotaro44 <shotaro2878@gmail.com>
2021-12-22 21:57:28 +0900

    bookmarksのルーティングを行なった

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
   root 'passages#show_all'
+  get 'users/:user_id/passages/:passage_id/bookmarks', to: 'bookmarks#show_passage_bookmarks'
 
   resources :users, path_names: { new: 'signup' } do
     resource :bookmarks, { only: %i[show] } # TODO: bookmarks用のコントローラ作成
-    resources :passages, { only: %i[create destroy show] }
+    resources :passages, { only: %i[create destroy show] } do
+      resource :bookmarks, { only: %i[create destroy] }
+    end
     resources :comments, { only: %i[create destroy show] }
   end
 
